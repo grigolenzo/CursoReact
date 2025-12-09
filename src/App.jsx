0de8227dc433b127d092bb5d1dc6d0671e0dec3c
enzo-martins <enzogmartins@gmail.com>
2025-12-09 15:20:23 -0300

    atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,30 @@ function App() {
     }
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => { 
+      if(task.id === taskId) {
+        return{...task, isCompleted: !task.isCompleted}
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks)
+  }
+
+  function onClickDeleteTask (taskId) {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="textl-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onClickDeleteTask={onClickDeleteTask} />
       </div>
     </div>
   );
